c4e21dfbee06c285621bdaee7fd7565ff3eb921a
bhatnagarpulkit <30912788+bhatnagarpulkit@users.noreply.github.com>
2017-08-14 21:46:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ element.innerHTML='New  Value';
 //move the image
 var image=document.getElementById('madi');
 img.onclick=function() {
-    img.style.marginleft='500px';
+    img.style.marginleft='100px';
 };
 
 
